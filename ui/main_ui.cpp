aee872e2a959bd87cfa0bb421d327852c4b9e41b
khare19yash <khareyash19@gmail.com>
2016-10-28 01:02:25 -0400

    Main User Interface

---
+++ ui/main_ui.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+cout<<"1. USER LOGIN"<<endl;
+cout<<"2. ADMIN LOGIN"<<endl;
+cout<<"3. DOCTOR LOGIN"<<endl;
+cout<<"4. EXIT"<<endl;
+
+return 0;
+}
+
+
